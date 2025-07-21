29ea9713fdde1376903765c4665c1ed5f2a6c4b0
MisterPi14 <ing.diegovargas14@gmail.com>
2025-07-21 13:11:38 -0600

    adding youtube api managing files

---
+++ Mabup2_Beta/Mabup2_Beta/Models/YouTubeSearchResult.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace Mabup2_Beta.Models
+{
+    public class Reproductor
+    {
+        public string Tema { get; set; }
+        public string Materia { get; set; }
+    }
+    public class YouTubeSearchResult
+    {
+        public string Tema { get; set; }
+        public List<YouTubeVideo> Videos { get; set; }
+    }
+    
+}
---
+++ Mabup2_Beta/Mabup2_Beta/Models/YouTubeVideo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mabup2_Beta.Models
+{
+    public class YouTubeVideo
+    {
+        public string VideoId { get; set; }
+        public string Title { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+
+}
+
